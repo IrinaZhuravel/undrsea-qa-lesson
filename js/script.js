f3deeedfc6b592125b93ebc8d20a896a468656fa
IrinaZhuravel <90681450+IrinaZhuravel@users.noreply.github.com>
2023-11-30 15:20:21 +0200

    form

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const burgerIcon = document.querySelector(".burger-icon");
   const mobileMenu = document.querySelector(".mobile-menu");
   const closeIcon = document.querySelector(".close-icon");
-
+  const submit = document.querySelector("#submit");
   // Открытие мобильного меню
   burgerIcon.addEventListener("click", function () {
     mobileMenu.style.display = "flex";
@@ -11,5 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
   // Закрытие мобильного меню
   closeIcon.addEventListener("click", function () {
     mobileMenu.style.display = "none";
+  });
+  submit.addEventListener("click", function () {
+    alert("Тип отправили в базу");
   });
 });
